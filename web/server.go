1d329861c2f9d149eba7ac34b89062847e4a5779
Julian Hoang <julian@julianhoang.com>
2018-05-24 12:53:08 -0400

    Add server mutex

--- web/server.go
+++ web/server.go
@@ -5,12 +5,15 @@ import (
 	"github.com/jephir/tradeblocks"
 	"github.com/jephir/tradeblocks/app"
 	"net/http"
+	"sync"
 )
 
 // Server implements a TradeBlocks node
 type Server struct {
+	mux *http.ServeMux
+
+	mu         sync.Mutex
 	blockstore *app.BlockStore
-	mux        *http.ServeMux
 }
 
 // NewServer allocates and returns a new server
@@ -36,7 +39,11 @@ func (s *Server) handleAccountBlock() http.HandlerFunc {
 		switch r.Method {
 		case "GET":
 			hash := r.FormValue("hash")
-			block, _ := s.blockstore.GetBlock(hash)
+			block, err := s.getBlock(hash)
+			if err != nil {
+				http.Error(w, "Couldn't get block.", http.StatusInternalServerError)
+				return
+			}
 			if block == nil {
 				http.Error(w, "No block found.", http.StatusBadRequest)
 				return
@@ -51,7 +58,7 @@ func (s *Server) handleAccountBlock() http.HandlerFunc {
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
-			if err := s.blockstore.AddBlock(&b); err != nil {
+			if err := s.addBlock(&b); err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
@@ -63,4 +70,16 @@ func (s *Server) handleAccountBlock() http.HandlerFunc {
 			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		}
 	}
+}
+
+func (s *Server) getBlock(hash string) (*tradeblocks.AccountBlock, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.blockstore.GetBlock(hash)
+}
+
+func (s *Server) addBlock(block *tradeblocks.AccountBlock) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.blockstore.AddBlock(block)
 }
