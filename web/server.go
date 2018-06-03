fc39471dd02a823c20275c72be9d58d3aa64e7ad
Hulnis <eric.parker13@gmail.com>
2018-06-03 13:41:09 -0400

    minor logging fix

--- web/server.go
+++ web/server.go
@@ -59,7 +59,7 @@ func (s *Server) handleAccountBlock() http.HandlerFunc {
 			hash := r.FormValue("hash")
 			block, err := s.service.getBlock(hash)
 			if err != nil {
-				log.Printf("error in handleAccountBlock GET1: %v Couldn't get block. \n")
+				log.Printf("error in handleAccountBlock GET1: Couldn't get block. \n")
 				http.Error(w, "Couldn't get block.", http.StatusInternalServerError)
 				return
 			}
